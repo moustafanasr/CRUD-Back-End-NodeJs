9f854bfe0ef2499e42254fd7ee58ccf0ce9e5668
Moustafa-M-Nasr <moustafanasr126@gmail.com>
2024-08-02 09:18:34 +0300

    "Refactored index.ts, removed views/articles.ejs and views/numbers.ejs"

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,25 @@ import express from 'express';
 import mongoose from 'mongoose';
 import authRoutes from './routes/auth';
 import taskRoutes from './routes/tasks';
-import { config } from 'dotenv';
-
-config();  // Load environment variables from .env file
 
 const app = express();
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://CRUD-System:102030456@moustafa-nasr.jdwhgxk.mongodb.net/?retryWrites=true&w=majority&appName=Moustafa-Nasr")
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => {
-    console.error('Failed to connect to MongoDB', err);
-  });
+const PORT = process.env.PORT || 4000; // Changed from 3000 to 3001
 
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000, Welcome to my project Moustafa M Nasr');
-});
+const MONGODB_URI = process.env.MONGODB_URI ||"mongodb+srv://CRUD-System:102030456@moustafa-nasr.jdwhgxk.mongodb.net/?retryWrites=true&w=majority&appName=Moustafa-Nasr";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}, Welcome to my project Moustafa M Nasr`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB', error);
+  });
